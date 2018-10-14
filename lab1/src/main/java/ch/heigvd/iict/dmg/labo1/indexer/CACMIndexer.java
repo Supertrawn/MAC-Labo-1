f1e10ad796420fbcc775774ead56ac659f10cfc6
ludelafo <ludovic.delafontaine@gmail.com>
2018-10-14 14:26:15 +0200

    Part one done.

--- lab1/src/main/java/ch/heigvd/iict/dmg/labo1/indexer/CACMIndexer.java
+++ lab1/src/main/java/ch/heigvd/iict/dmg/labo1/indexer/CACMIndexer.java
@@ -2,7 +2,9 @@ package ch.heigvd.iict.dmg.labo1.indexer;
 
 import ch.heigvd.iict.dmg.labo1.parsers.ParserListener;
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.document.Document;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.*;
+import org.apache.lucene.index.IndexOptions;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.IndexWriterConfig.OpenMode;
@@ -13,45 +15,83 @@ import org.apache.lucene.store.FSDirectory;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.Objects;
 
 public class CACMIndexer implements ParserListener {
-	
-	private Directory 	dir 			= null;
-	private IndexWriter indexWriter 	= null;
-	
-	private Analyzer 	analyzer 		= null;
-	private Similarity 	similarity 		= null;
-	
+
+	private Directory dir = null;
+	private IndexWriter indexWriter = null;
+
+	private Analyzer analyzer = null;
+	private Similarity similarity = null;
+
 	public CACMIndexer(Analyzer analyzer, Similarity similarity) {
 		this.analyzer = analyzer;
 		this.similarity = similarity;
 	}
-	
+
 	public void openIndex() {
 		// 1.2. create an index writer config
 		IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
 		iwc.setOpenMode(OpenMode.CREATE); // create and replace existing index
-		iwc.setUseCompoundFile(false); // not pack newly written segments in a compound file: 
+		iwc.setUseCompoundFile(false); // not pack newly written segments in a compound file:
 		//keep all segments of index separately on disk
-		if(similarity != null)
+		if (similarity != null)
 			iwc.setSimilarity(similarity);
 		// 1.3. create index writer
 		Path path = FileSystems.getDefault().getPath("index");
 		try {
 			this.dir = FSDirectory.open(path);
 			this.indexWriter = new IndexWriter(dir, iwc);
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
 	public void onNewDocument(Long id, String authors, String title, String summary) {
 		Document doc = new Document();
 
-		// TODO student: add to the document "doc" the fields given in
-		// parameters. You job is to use the right Field and FieldType
-		// for these parameters.
+		FieldType fieldType = new FieldType();
+
+		fieldType.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS);
+		fieldType.setTokenized(true);
+		fieldType.setStoreTermVectors(true);
+		fieldType.setStoreTermVectorPositions(true);
+		fieldType.setStoreTermVectorOffsets(true);
+		fieldType.freeze();
+
+		String[] authorsList = authors.split(";");
+
+		// Create the fields
+		Field idField = new LongPoint("id", id);
+		Field titleField = new Field("title", title, fieldType);
+		Field authorField;
+		Field summaryField;
+
+		// Add the fields to the document
+		doc.add(idField);
+		doc.add(titleField);
+
+		if (Objects.isNull(authors)) {
+			authorField = new StringField("author", "", Field.Store.YES);
+			doc.add(authorField);
+		} else {
+			// Add all the authors
+			for (String authorName : authorsList) {
+				authorField = new StringField("author", authorName, Field.Store.YES);
+				doc.add(authorField);
+			}
+		}
+
+		if (Objects.isNull(summary)) {
+			summaryField = new Field("summary", "", fieldType);
+		} else {
+			summaryField = new Field("summary", summary, fieldType);
+		}
+
+		doc.add(summaryField);
 
 		try {
 			this.indexWriter.addDocument(doc);
@@ -59,12 +99,16 @@ public class CACMIndexer implements ParserListener {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void finalizeIndex() {
-		if(this.indexWriter != null)
-			try { this.indexWriter.close(); } catch(IOException e) { /* BEST EFFORT */ }
-		if(this.dir != null)
-			try { this.dir.close(); } catch(IOException e) { /* BEST EFFORT */ }
+		if (this.indexWriter != null)
+			try {
+				this.indexWriter.close();
+			} catch (IOException e) { /* BEST EFFORT */ }
+		if (this.dir != null)
+			try {
+				this.dir.close();
+			} catch (IOException e) { /* BEST EFFORT */ }
 	}
-	
+
 }
